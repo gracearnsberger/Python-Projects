767539b26ba6b29775da18aedaf6bc0c906c3135
gracearnsberger <50274639+gracearnsberger@users.noreply.github.com>
2020-06-24 10:32:34 -0600

    Attributes Practice
    Practicing attributes in classes using python

---
+++ attributes.py
@@ -0,0 +1,13 @@
+class Person:
+    name = "Harris"
+    personal_ID = "45960"
+    phone_number = "123-456-7890 "
+    email = "n/a"
+
+class Teacher(Person):
+    salary = "33,000"
+    subject = "Science"
+
+class Student(Person):
+    grade = "Junior"
+    age = "16"
