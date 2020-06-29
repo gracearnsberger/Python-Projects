affeeeda31386719d59893bc5d9eaaa9e3e6bab3
gracearnsberger <50274639+gracearnsberger@users.noreply.github.com>
2020-06-29 10:54:59 -0600

    Atrribute Practice

--- attributes.py
+++ attributes.py
@@ -1,13 +1,52 @@
 class Person:
     name = "Harris"
-    personal_ID = "45960"
-    phone_number = "123-456-7890 "
-    email = "n/a"
+    ID = "45960"
+    number = "123-456-7890 "
+
+    def getLoginInfo(self):
+        entry_name = input("Enter your name here: ")
+        entry_ID = input("Enter your ID# here: ")
+        entry_number = input("Enter your Phone# here: ")
+        if (entry_name == self.name and entry_ID == self.ID):
+            print("Welcome back, {}!".format(entry_name))
+        else:
+            print("The ID# or name is incorrect.")
 
 class Teacher(Person):
     salary = "33,000"
-    subject = "Science"
+    ID = "Josh22"
+    emp_num = "4583"
+
+    def getLoginInfo(self):
+        entry_name = input("Enter your name here: ")
+        entry_ID = input("Enter your ID# here: ")
+        entry_empnum = input("Enter you employee pin# here: ")
+        if (entry_ID == self.ID and entry_empnum == self.empnum):
+            print("Welcome back, {}!".format(entry_name))
+        else:
+            print("The employee number or ID is incorrect.")
 
 class Student(Person):
     grade = "Junior"
-    age = "16"
+    period = "1"
+    password = "happy123"
+    
+    
+    def getLoginInfo(self):
+        entry_name = input("Enter your name here: ")
+        entry_period = input("Enter your period# here: ")
+        entry_password = input("Enter your password here: ")
+        if (entry_period == self.period and entry_password == self.password):
+            print("Welcome back, {}!".format(entry_name))
+        else:
+            print("The period number or password is incorrect.")
+
+
+person = Person()
+person.getLoginInfo()
+
+teacher = Teacher()
+teacher.getLoginInfo()
+
+student = Student()
+student.getLoginInfo()
