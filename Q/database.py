31fbd8803dc687e2184ff5cc829869e4799a3ec7
gracearnsberger <50274639+gracearnsberger@users.noreply.github.com>
2020-07-02 10:11:12 -0600

    Update database.py

--- Q/database.py
+++ Q/database.py
@@ -2,11 +2,18 @@ import sqlite3
 import os
 
 conn = sqlite3.connect("python.db")
+cursor = conn.cursor()
+cursor.execute("""CREATE TABLE IF NOT EXISTS tbl_files
+                 (id integer primary key, name varchar(50))
+          """)
 
-with conn:
-    fileList = ('')
-    cur = conn.cursor()
-    cur.execute("SELECT col_filename, col_ext FROM fileList WHERE col_ext = '.txt'")
-    varFile = cur.fetchall()
-    
+## change the path
+path = 'C:\Python Projects\Q'
 
+for file in os.listdir(path):
+    if file.endswith('.txt'):
+        print(file)
+        cursor.execute("""insert into tbl_files (name) values ('{}') """.format(file))
+        conn.commit()
+
+conn.close()      
